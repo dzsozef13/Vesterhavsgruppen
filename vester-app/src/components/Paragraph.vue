76b97aea1c0b69349be6bdf89a0c47326cb458a9
dzsozef13 <punkjoad@gmail.com>
2021-05-26 12:22:48 +0200

    components imported

---
+++ vester-app/src/components/Paragraph.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container id="paragraph">
+        <h3>{{ prgText }}</h3>
+    </v-container>
+</template>
+
+<script>
+export default {
+    props: ['prgText']
+}
+</script>
+
+<style lang="scss">
+    #paragraph {
+        text-align: left;
+        margin: 0 0 32px 0;
+        padding: 0;
+        max-width: 400px;
+        h3 {
+            font-family: 'Poppins', sans-serif;
+            font-size: 18px;
+            font-weight: 400;
+        }
+    }
+</style>
+
--- vester-app/src/views/Home.vue
+++ vester-app/src/views/Home.vue
@@ -1,18 +1,68 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<v-app id="home">
+
+  <v-container class="section-1" style="max-width: unset; margin:0;">
+    <v-row>
+      <v-col class="asd">
+        <Title ttlText="Welkommen"/>
+        <Paragraph prgText="My challenge was to help Rubea, a local enamelist to create a channel to sell her products and to boost her SoMe reach."/>
+        <SquareButton btnText="Bliv Spejder" route="/about"/>
+      </v-col>
+      <v-col>
+        <div>img</div>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container class="section-1" style="max-width: unset; margin:0;">
+    <Title ttlText="About"/>
+    <Paragraph prgText="My challenge was to help Rubea, a local enamelist to create a channel to sell her products and to boost her SoMe reach."/>
+    <SquareButton btnText="Bliv Spejder" route="/about"/>
+  </v-container>
+
+</v-app>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import Title from '@/components/Title.vue'
+import Paragraph from '@/components/Paragraph.vue'
+import SquareButton from '@/components/SquareButton.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    // HelloWorld
+    Title,
+    Paragraph,
+    SquareButton
   }
 }
 </script>
+
+<style lang="scss" scoped>
+
+@mixin section {
+  display: flex;
+  align-items: left;
+  justify-content: center;
+  min-height: 100vh;
+  padding: 24px 16vw 24px 16vw;
+  margin: 0;
+}
+
+.section-1 {
+  @include section();
+  background-color: #eee;
+  color: rgb(35, 33, 56);
+}
+
+.asd {
+  display: flex;
+  flex-direction: column;
+  justify-items: center;
+  height: max-content;
+}
+
+</style>

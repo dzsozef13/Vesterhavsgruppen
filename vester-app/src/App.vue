b997f40a3ce988cdf3ec7a6cb7f4b58dfe95aefd
dzsozef13 <punkjoad@gmail.com>
2021-05-27 21:25:58 +0200

    asd

--- vester-app/src/App.vue
+++ vester-app/src/App.vue
@@ -30,13 +30,13 @@
 @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap');
 
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+// #app {
+  // font-family: Avenir, Helvetica, Arial, sans-serif;
+  // -webkit-font-smoothing: antialiased;
+  // -moz-osx-font-smoothing: grayscale;
+  // text-align: center;
+  // color: #2c3e50;
+// }
 
 #nav {
   display: flex;

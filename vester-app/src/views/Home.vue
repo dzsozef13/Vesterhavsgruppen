8cc2c87ccb5270289a521eb565a0c3f8b350ebab
kos3l <71730597+kos3l@users.noreply.github.com>
2021-05-27 18:37:16 +0200

    dark about

--- vester-app/src/views/Home.vue
+++ vester-app/src/views/Home.vue
@@ -3,7 +3,7 @@
 
   <v-container class="section-1" style="max-width: unset; margin:0;">
     <v-row id="mainrow">
-      <v-col class="asd" >
+      <v-col class="asd">
         <Title ttlText="Welkommen"/>
         <Paragraph prgText="My challenge was to help Rubea, a local enamelist to create a channel to sell her products and to boost her SoMe reach."/>
         <SquareButton btnText="Bliv Spejder" route="/about"/>
@@ -14,21 +14,21 @@
     </v-row>
   </v-container>
 
-  <v-container style="max-width: unset; margin:0; padding:0;">
+  <v-container style="max-width: unset; margin:0; padding:0;" id="video">
     <v-row lg="12" style="margin:0;" >
       <v-col lg="12" style="padding:0;">
-        <v-img :src="require('../assets/Thumb.jpg')"></v-img>
+        <v-img :src="require('../assets/Thumb.jpg')" id="video"></v-img>
       </v-col>
     </v-row>
   </v-container>
 
-  <v-container class="section-1" style="max-width: unset; margin:0;">
-    <v-row>
-      <v-col>
+  <v-container class="section-1" style="max-width: unset; margin:0;" id="dark">
+    <v-row >
+      <v-col id="about">
         <Title ttlText="About"/>
         <Paragraph prgText="My challenge was to help Rubea, a local enamelist to create a channel to sell her products and to boost her SoMe reach."/>
         <SquareButton btnText="Bliv Spejder" route="/about"/>
-      </v-col>
+     </v-col>
     </v-row>
   </v-container>
 
@@ -83,4 +83,16 @@ export default {
 
 }
 
+#about{
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+}
+#dark{
+  background-color: rgb(35, 33, 56) ;
+  color: #eee;
+}
+#video{
+  min-height: 100vh;
+}
 </style>
